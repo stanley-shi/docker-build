42feb638e818a6d1577965d9b6f900ff372d134b
Stanley Shi <demshi@ebay.com>
2015-06-01 17:43:44 +0800

    add tomcat manager-gui permission

--- mario/setup.sh
+++ mario/setup.sh
@@ -5,3 +5,17 @@ sed -i  's/127.0.0.1/0.0.0.0/' /etc/mysql/my.cnf
 service mysql start
 mysqladmin password 'dangerous'
 mysql -u root --password=dangerous -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+
+
+### setup tomcat gui password
+cat > /opt/apache-tomcat-6.0.44/conf/tomcat-users.xml <<EOF
+
+<tomcat-users>
+  <role rolename="tomcat"/>
+  <role rolename="admin-gui"/>
+  <role rolename="manager-gui"/>
+  <user username="tomcat" password="tomcat" roles="tomcat,admin-gui,manager-gui"/>
+</tomcat-users>
+
+
+EOF
